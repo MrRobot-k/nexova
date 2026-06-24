07329416730c33889bd95cea25a574e5e9598619
MrRobot-k <esdraswarzone@gmail.com>
2026-06-23 19:01:48 -0600

    add @vercel/speed-insights integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+import speedInsights from '@vercel/speed-insights/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xolventa.vercel.app',
-  integrations: [react()],
+  integrations: [react(), speedInsights()],
 
   vite: {
     plugins: [tailwindcss()]
